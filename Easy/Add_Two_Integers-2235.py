1ee648dcc891fe4ee178bafba31646c705ee8083
salvien-code <salviensky@gmail.com>
2022-07-18 10:42:41 +0100

    Completed leetcode questions 2235, 1480, 1672.

---
+++ Easy/Add_Two_Integers-2235.py
@@ -0,0 +1,7 @@
+class Solution:
+    """
+    sum -> Time { O(1), θ(1), Ω(1) } and Space { O(1), θ(1), Ω(1) }
+    """
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
+        
---
+++ Easy/Richest_Customer_Wealth-1672.py
@@ -0,0 +1,19 @@
+import sys
+
+class Solution:
+    """
+    maximumWealth   -> Time { O(n^2), θ(n^2), Ω(1) } and Space { O(1), θ(1), Ω(1) }
+    """
+    def maximumWealth(self, accounts: list[list[int]]) -> int:
+        max_wealth = -sys.maxsize-1
+        
+        for i in range(len(accounts)):
+            local_wealth = 0
+            
+            for j in range(len(accounts[i])):
+                local_wealth += accounts[i][j]
+            
+            if local_wealth > max_wealth:
+                max_wealth = local_wealth
+                
+        return max_wealth
