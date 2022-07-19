832936eb64831af6f33b862f101a6667cfda4738
salvien-code <salviensky@gmail.com>
2022-07-19 07:26:46 +0100

    Added question 2248

---
+++ Easy/2248-Intersection_of_Multiple_Arrays.py
@@ -0,0 +1,13 @@
+class Solution:
+    """
+    intersection    -> Time { O(n), θ(n), Ω(1) } and Space { O(1), θ(1), Ω(1) }
+    """
+
+    def intersection(self, nums: list[list[int]]) -> list[int]:
+
+        result = set(nums[0])
+
+        for num in nums[1:]:
+            result.intersection_update(num)
+
+        return sorted(result)
