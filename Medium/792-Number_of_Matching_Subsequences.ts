38e34bad908b34ed28b4483440d94e31b73aa2bb
Salvien-code <salviensky@gmail.com>
2022-07-21 20:41:35 +0100

    Still working on question 792

--- Medium/792-Number_of_Matching_Subsequences.ts
+++ Medium/792-Number_of_Matching_Subsequences.ts
@@ -1,6 +1,24 @@
 function numMatchingSubseq(s: string, words: string[]): number {
+  if (s.length == 0) return 0;
+  subseq = 0;
+  
+  for (word in words) {
+    for (i = 0; i < s.length; i++) {
+      if (s[i] == word[0] && i != s.length-1) {
+        subset += verifySubseq(s.slice(i+1), word.slice(1))
+      }
+    }
+  }
+  
+  return subseq;
+}
+
+function verifySubseq(supseq: string, word: string): number {
   
 }
 
-// For word in words
-// if word[0] is in s, pass word[1:] to verify
+
+// Loop through supseq and check if word[0] is present
+// if present and i!=s.length-1, 
+// return verifySubseq(s.slice(i+1), word.slice(1))
+// return 0
