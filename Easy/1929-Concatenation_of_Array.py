dd24cbe86b89a86c22ab865226f050b81af76fa7
salvien-code <salviensky@gmail.com>
2022-07-25 08:51:33 +0100

    Solved questions 1693, 1929, 1393

--- Easy/1929-Concatenation_of_Array.py
+++ Easy/1929-Concatenation_of_Array.py
@@ -1,3 +1,5 @@
 class Solution:
+    # getConcatenation  -> Time { O(1), θ(1), Ω(1) } and Space { O(1), θ(1), Ω(1) }
+
     def getConcatenation(self, nums: list[int]) -> list[int]:
-        return nums+nums
+        return nums + nums
