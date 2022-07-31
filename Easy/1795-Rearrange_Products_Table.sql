1a1420b306f16ce38ea8ceb376437cfd5e7431a7
salvien-code <salviensky@gmail.com>
2022-07-31 10:07:11 +0100

    Formatted question 1795

--- Easy/1795-Rearrange_Products_Table.sql
+++ Easy/1795-Rearrange_Products_Table.sql
@@ -1,33 +1,29 @@
-SELECT 
-    product_id, 
-    'store1' AS 'store', 
+SELECT
+    product_id,
+    'store1' AS 'store',
     store1 AS price
-FROM 
-    Products 
-WHERE 
+FROM
+    Products
+WHERE
     store1 is NOT NULL
-
 UNION
-
-SELECT 
-    product_id, 
-    'store2' AS 'store', 
+SELECT
+    product_id,
+    'store2' AS 'store',
     store2 AS price
-FROM 
-    Products 
-WHERE 
+FROM
+    Products
+WHERE
     store2 is NOT NULL
-
-UNION 
-
-SELECT 
-    product_id, 
-    'store3' AS 'store', 
+UNION
+SELECT
+    product_id,
+    'store3' AS 'store',
     store3 AS price
-FROM 
-    Products 
-WHERE 
+FROM
+    Products
+WHERE
     store3 is NOT NULL
-
 ORDER BY
-    product_id, store
+    product_id,
+    store
