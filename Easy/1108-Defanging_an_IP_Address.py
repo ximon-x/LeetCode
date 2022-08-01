626377202fb20a9c6ae1b031d56b2934127d5064
salvien-code <salviensky@gmail.com>
2022-08-01 13:51:38 +0100

    Still attempting question 62

--- Easy/1108-Defanging_an_IP_Address.py
+++ Easy/1108-Defanging_an_IP_Address.py
@@ -1,3 +1,5 @@
 class Solution:
+    # defangIPaddr  -> Time { O(n), θ(n), Ω(1) } and Space { O(n), θ(n), Ω(n) }
+
     def defangIPaddr(self, address: str) -> str:
         return address.replace(".", "[.]")
--- WIP/62-Unique_Paths.ts
+++ WIP/62-Unique_Paths.ts
@@ -1,5 +1,23 @@
 function uniquePaths(m: number, n: number): number {
+  let memo: number = 0;
+
+  const pathFinder = (i: number, j: number, memo: number): number => {
+    if (i === 0 && j === 0) return memo;
+    console.log("Returning 1");
+
+    if (i !== 0) {
+      memo += pathFinder(i - 1, j, memo);
+    }
+
+    if (j !== 0) {
+      memo += pathFinder(i, j - 1, memo);
+    }
     
+    return memo;
+  };
+
+  memo = pathFinder(m, n, memo);
+  return memo;
 }
 
-// Its a factorial problem.
+console.log(uniquePaths(3, 7));
