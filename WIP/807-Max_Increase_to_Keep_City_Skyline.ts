1690b7b6c72a8e82b0cdb35d14c662f13223a954
Simon Samuel <salviensky@gmail.com>
2022-09-07 08:42:54 +0100

    Solved question 807

---
+++ WIP/807-Max_Increase_to_Keep_City_Skyline.ts
@@ -0,0 +1,38 @@
+function maxIncreaseKeepingSkyline(grid: number[][]): number {
+  let col: number[] = [];
+  let row: number[] = [];
+
+  for (let i = 0; i < grid.length; i++) {
+    let rowMax = 0;
+    row[i] = 0;
+
+    for (let j = 0; j < grid[i].length; j++) {
+      if (rowMax < grid[i][j]) {
+        row[i] = grid[i][j];
+        rowMax = grid[i][j];
+      }
+    }
+  }
+
+  for (let i = 0; i < grid.length; i++) {
+    let colMax = 0;
+    col[i] = 0;
+
+    for (let j = 0; j < grid[i].length; j++) {
+      if (colMax < grid[j][i]) {
+        col[i] = grid[j][i];
+        colMax = grid[j][i];
+      }
+    }
+  }
+
+  let ans = 0;
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      let diff = Math.min(row[i], col[j]) - grid[i][j];
+      ans += diff;
+    }
+  }
+  return ans;
+}
