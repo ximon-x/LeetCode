98ce2a66af7b8018811a074d4b9b07845dc038f7
Simon Samuel <salviensky@gmail.com>
2022-09-14 09:03:49 +0100

    Saving progress for 1282

---
+++ WIP/1282-Group_the_People_Given_the_Group_Size_They_Belong_To.ts
@@ -0,0 +1,17 @@
+type Hashmap = {
+  [index: number]: number[];
+};
+
+function groupThePeople(groupSizes: number[]): number[][] {
+  let result: number[][];
+  let groups: Hashmap;
+
+  for (let i = 0; i < groupSizes.length; ++i) {
+    let list: number[] = groups.;
+  } return result;
+}
+// Saving progress.
+// Current Solution from: Saved leetCode java problem
+
+// Potential next solution
+// https://leetcode.com/problems/group-the-people-given-the-group-size-they-belong-to/discuss/446335/Java-Short-Concise-and-Easy-Using-HashMap
