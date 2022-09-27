17838f8c2648fc718b9ed842bede45a3b75ce671
Simon Samuel <salviensky@gmail.com>
2022-09-27 08:31:43 +0100

    Solved 1315. Still need to understand algorithm.

--- WIP/1315-Sum_of_Nodes_with_Even-Valued_Grandparents.ts
+++ WIP/1315-Sum_of_Nodes_with_Even-Valued_Grandparents.ts
@@ -10,32 +10,19 @@ class TreeNode {
 }
 
 function sumEvenGrandparent(root: TreeNode | null): number {
-  let result: number = 0;
-
-  function grandChildTraverse(node: TreeNode | null, depth: number): number {
-    if (node === null) return 0;
-    let sum: number = 0;
-
-    sum += grandChildTraverse(node.left, depth + 1);
-
-    return sum;
-  }
-
-  result += grandChildTraverse(root, 0);
-  return result;
+  return grandChildTraverse(root, 1, 1);
 }
 
-// Problem
-// Return sum of grand parent nodes.
+function grandChildTraverse(
+  node: TreeNode | null,
+  parent: number,
+  grandParent: number
+): number {
+  if (node === null) return 0;
 
-// Solution
-// Check if a node is even,
-// If it is even, perform a depth first search to be sure if has a node in the second generation as true.
-// Return node if true
-// Add node to result
-// return result
-
-// Specific problems
-// 1. We need a way for a node to know if a grandchild exists.
-// Specific solutions
-// 2.
+  return (
+    grandChildTraverse(node.left, node.val, parent) +
+    grandChildTraverse(node.right, node.val, parent) +
+    (grandParent % 2 == 0 ? node.val : 0)
+  );
+}
