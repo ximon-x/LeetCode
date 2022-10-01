47648ea9801cd0c6c87eae9d11bb674f31703321
Simon Samuel <salviensky@gmail.com>
2022-10-01 09:17:37 +0100

    Solved question 1038

--- WIP/1038-Binary_Search_Tree_To_Greater_Sum_Tree.ts
+++ WIP/1038-Binary_Search_Tree_To_Greater_Sum_Tree.ts
@@ -1,24 +1,20 @@
 class TreeNode {
-  val: number;
-  left: TreeNode | null;
-  right: TreeNode | null;
-  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.left = left === undefined ? null : left;
-    this.right = right === undefined ? null : right;
-  }
+	val: number;
+	left: TreeNode | null;
+	right: TreeNode | null;
+	constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+		this.val = val === undefined ? 0 : val;
+		this.left = left === undefined ? null : left;
+		this.right = right === undefined ? null : right;
+	}
 }
+
+let pre: number = 0;
 
 function bstToGst(root: TreeNode | null): TreeNode | null {
-  return root;
+	if (root.right != null) bstToGst(root.right);
+	pre = root.val = pre + root.val;
+
+	if (root.left != null) bstToGst(root.left);
+	return root;
 }
-
-function increment(node: TreeNode | null): number {
-  if (node === null) return 0;
-
-  node.val += node.right ? node.right.val : 0;
-  return 0;
-}
-
-// Wrong solution:
-// Question is about increasing by all keys not by keys in the right section.
