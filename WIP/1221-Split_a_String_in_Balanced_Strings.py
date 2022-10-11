0b3e90e5622ce6c8f0b688afa96bd5a057fdc5f7
Simon Samuel <salviensky@gmail.com>
2022-10-11 08:14:07 +0100

    Solved question 1221

--- WIP/1221-Split_a_String_in_Balanced_Strings.py
+++ WIP/1221-Split_a_String_in_Balanced_Strings.py
@@ -1,32 +1,16 @@
-class solution:
+class Solution:
     def balancedStringSplit(self, s: str) -> int:
-        pr = 0
-        pl = 0
-        result = []
+        balance = 0
+        result = 0
 
         for i in range(len(s)):
-            if s[i] == "R":
-                pr += 1
+            if s[i] == "L":
+                balance += 1
 
             else:
-                pl += 1
+                balance -= 1
 
-        return len(result)
+            if i != 0 and balance == 0:
+                result += 1
 
-
-"""
-Problem: Split string into balanced Substrings
-
-Solution 1:
-    1. Create 2 pointers, set them both to 0
-    2. Traverse linearly with the "R" pointer until a "L" is encountered
-    3. Keep track of the number of "R" encountered in a variable
-    4. Traverse linearly with the "L" pointer from the current location of
-        pointer "R" until a "R" is encountered
-    5. Keep track of the number of "L" encountered in a variable
-    6. Compare number of "R" to number of "L", if equal, result.append(str("R" * numR + "L" * numR))
-
-Current Hindrances:
-    1. How to traverse with the pointers correctly
-"""
-
+        return result
