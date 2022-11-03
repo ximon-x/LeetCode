31d8cca812e19525136dcf2d926740354ded62a4
Simon Samuel <salviensky@gmail.com>
2022-11-03 08:32:11 +0100

    Still working on problem 2273

--- WIP/2273-Largest_Local_Values_in_a_Matrix.py
+++ WIP/2273-Largest_Local_Values_in_a_Matrix.py
@@ -1,11 +1,16 @@
 class Solution:
     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        answer = [][]
+
+        # Find all pivots and make the current one the local_max
         for i in range(1, len(grid)-1):
             for j in range(1, len(grid[i])-1):
-                # Assigning current pivot as the current max
                 local_max = grid[i][j]
 
-                # Iterate around pivot and add max value to max_local
+                # Iterate around pivot and find max value   
+                for k in range(i-1, i+2):
+                    for l in range(j-1, j+2): 
+                        if grid[k][l] > local_max:
+                            local_max = grid[k][l]
 
-
-
+         # Figure out how to store the values in answer
