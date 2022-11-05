f97d868789186cb0046c8227fafc53765cadd41a
Simon Samuel <salviensky@gmail.com>
2022-11-05 08:30:52 +0100

    Solving question 2129. Almost done

---
+++ WIP/2129-Execution_of_All_Suffix_Instructions_Staying_in_a_Grid.ts
@@ -0,0 +1,32 @@
+function executeInstructions(n: number, startPos: number[], s: string): number[] {
+	let answer: number[] = [];
+
+	// Iterate through s
+	// 		goodSteps = 0
+	//
+	// 		Iterate from s[i] to s.length
+	// 			switch(s[i])
+	// 				case "L" {
+	// 					startPos[1]++
+	// 					if startPos[1] > n-1 {
+	// 						break
+	// 					} else {
+	// 						goodSteps++
+	// 					}
+	// 				}
+	// 				case "R" {
+	// 					startPos[1]--
+	// 				}
+	// 				case "U" {
+	// 					startPos[0]++
+	// 				}
+	// 				case "L" {
+	// 					startPos[0]--
+	// 				}
+
+
+
+
+
+
+}
