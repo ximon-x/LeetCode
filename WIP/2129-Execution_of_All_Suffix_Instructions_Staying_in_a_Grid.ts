070a8bea8b91d78587f5f5946107c2ff4830706c
Simon Samuel <salviensky@gmail.com>
2022-11-07 08:32:14 +0100

    Still have logical errors

--- WIP/2129-Execution_of_All_Suffix_Instructions_Staying_in_a_Grid.ts
+++ WIP/2129-Execution_of_All_Suffix_Instructions_Staying_in_a_Grid.ts
@@ -1,32 +1,58 @@
-function executeInstructions(n: number, startPos: number[], s: string): number[] {
-	let answer: number[] = [];
+function executeInstructions(
+  n: number,
+  startPos: number[],
+  s: string
+): number[] {
+  let answer: number[] = [];
 
-	// Iterate through s
-	// 		goodSteps = 0
-	//
-	// 		Iterate from s[i] to s.length
-	// 			switch(s[i])
-	// 				case "L" {
-	// 					startPos[1]++
-	// 					if startPos[1] > n-1 {
-	// 						break
-	// 					} else {
-	// 						goodSteps++
-	// 					}
-	// 				}
-	// 				case "R" {
-	// 					startPos[1]--
-	// 				}
-	// 				case "U" {
-	// 					startPos[0]++
-	// 				}
-	// 				case "L" {
-	// 					startPos[0]--
-	// 				}
+  for (let i = 0; i < s.length; i++) {
+    let currPos = startPos;
+    let goodSteps = 0;
 
+    for (let j = i; j < s.length; j++) {
+      switch (s[j]) {
+        case "L": {
+          currPos[1]--;
+          if (currPos[1] < 0) {
+            break;
+          } else {
+            goodSteps++;
+          }
+        }
 
+        case "R": {
+          currPos[1]++;
+          if (currPos[1] > n - 1) {
+            break;
+          } else {
+            goodSteps++;
+          }
+        }
 
+        case "U": {
+          currPos[0]--;
+          if (currPos[0] < 0) {
+            break;
+          } else {
+            goodSteps++;
+          }
+        }
 
+        case "D": {
+          currPos[0]--;
+          if (currPos[0] > n - 1) {
+            break;
+          } else {
+            goodSteps++;
+          }
+        }
+      }
+    }
 
+    answer.push(goodSteps);
+  }
 
+  return answer;
 }
+
+console.log(executeInstructions(3, [0, 1], "RRDDLU"));
