147845b3bcc6baf33ce22105d1b1b3a3fb21d506
Simon Samuel <salviensky@gmail.com>
2022-11-10 08:31:00 +0100

    Attempting question 1329

---
+++ WIP/1329-Sort_the_Matrix_Diagonally.ts
@@ -0,0 +1,8 @@
+function diagonalSort (mat: number[][]): number[][] {
+
+}
+
+// Algorithm:
+// Collect all elements of a diagonal in a map using the diagonial index (i - j) as key
+// Sort the map
+// Restore diagonal in matrix
