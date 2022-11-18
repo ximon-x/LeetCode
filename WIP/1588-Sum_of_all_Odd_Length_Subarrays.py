9ec2113dd6de0503fefd3dd7c0d9c8377ab39aa1
Simon Samuel <salviensky@gmail.com>
2022-11-18 08:28:51 +0100

    Solving question 1588

---
+++ WIP/1588-Sum_of_all_Odd_Length_Subarrays.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        sum = 0
+
+        for i in range (1, len(arr)+1, 2):
+            for j in range(0, len(arr)):
+                if j + i >= len(arr):
+                    break
+
+                for k in range(0, i):
+                    sum += arr[j + k]
+
+        return sum
