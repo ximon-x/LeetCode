0a1044ba565c896b15629ca238f1d5b00203c027
Simon Samuel <salviensky@gmail.com>
2022-11-21 08:14:53 +0100

    Solved question 1791

---
+++ Easy/1791-Find_Center_of_Star_Graph.py
@@ -0,0 +1,12 @@
+class Solution: 
+    def findCenter(self, edges: List[List[int]]) -> int:
+        # findCenter    ->  Time { O(n), θ(n), Ω(1) } and Space { O(1), θ(1), Ω(1) }
+
+        item = set(edges[0])
+
+        for i in range (1, len(edges)):
+            item = set(edges[i]) & set(item)
+
+        item = list(item)
+
+        return item[0]
