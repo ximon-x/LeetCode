89de1c22dcf47ceab29f0ae4dede410f117d5578
Simon Samuel <salviensky@gmail.com>
2022-11-21 08:29:17 +0100

    Solved question 2469

---
+++ Easy/2469-Convert_the_Temperature.py
@@ -0,0 +1,6 @@
+class Solution:
+    def convertTemperature (self, celsius: float) -> List[float]:
+        # convertTemperature    ->  Time { O(1), θ(1), Ω(1) } and Space { O(1), θ(1), Ω(1) }
+
+        return [round(celsius + 273.15, 5), round(celsius * 1.80 + 32.00)]
+
