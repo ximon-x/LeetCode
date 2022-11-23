01ef8dff9b6824ff19900c9092a9869b1f27d8bd
Simon Samuel <salviensky@gmail.com>
2022-11-23 09:02:51 +0100

    Solved question 1688

---
+++ Easy/1688-Count_of_Matches_in_Tournament.py
@@ -0,0 +1,15 @@
+class Solution: 
+    def numberOfMatches(self, n: int) -> int:
+        # numberOfMatches  -> Time { O(log(n)), θ(log(n)), Ω(n) } and Space { O(1), θ(1), Ω(1) }
+        result = 0
+
+        while n != 1:
+            result += n // 2
+
+            if n % 2 == 0:
+                n = n / 2
+            else:
+                n = (n - 1) / 2 + 1
+
+        return int(result)
+
