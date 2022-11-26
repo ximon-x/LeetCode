7cd4a46f9f26e6e2b8f85392eb1c53b8a05068f8
Simon Samuel <salviensky@gmail.com>
2022-11-26 08:20:57 +0100

    Solved question 1614. Don't understand solution.

--- Easy/1614-Maximum_Nesting_Depth_of_the_Parentheses.py
+++ Easy/1614-Maximum_Nesting_Depth_of_the_Parentheses.py
@@ -2,7 +2,7 @@ class Stack:
     items = []
 
     def init(self, item):
-        items.push(item)
+        self.items.push(item)
 
     def pop(self):
         if len(self.items) == 0:
@@ -12,32 +12,34 @@ class Stack:
             del self.items[-1]
             return value
 
-    def push(self, item)
-        self.items.push(item)
+    def push(self, item):
+        self.items.append(item)
 
     def peek(self):
         if len(self.items) == 0:
             raise IndexError
         else:
-            return 
-    
-    def size(self)
+            return
+
+    def size(self):
         return len(self.items)
+
 
 class Solution:
     def maxDepth(self, s: str) -> int:
+        values = Stack()
         result = 0
 
         if len(s) == 0:
             return result
 
         else:
-            values = Stack()
-
             for char in s:
                 if char == "(":
                     values.push("(")
-        
+                    result = max(result, values.size())
+
+                elif char == ")":
+                    values.pop()
 
         return result
-
