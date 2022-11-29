899327b233cfdc760489bc83cefccd9ca4b44a03
Simon Samuel <salviensky@gmail.com>
2022-11-29 08:31:41 +0100

    Solving question 2225

--- WIP/2225_Find_Players_With_Zero_or_one_Losses.ts
+++ WIP/2225_Find_Players_With_Zero_or_one_Losses.ts
@@ -1,19 +1,31 @@
 function findWinners(matches: number[][]): number[][] {
   let result: number[][];
 
+  let winners: number[] = [];
+  let losers: number[] = [];
+
   let noLossWinners: number[] = [];
   let oneLossWinners: number[] = [];
 
   for (let i = 0; i < matches.length; i++) {
-    let losses = 0;
-    for (let j = i + 1; j < matches.length; j++) {
-      if (
-        noLossWinners.includes(matches[i][0]) ||
-        oneLossWinners.includes(matches[i][0])
-      )
-        continue;
-      else {
+    winners.push(matches[i][0]);
+    losers.push(matches[i][1]);
+  }
+
+  for (let i = 0; i < winners.length; i++) {
+    let count = 0;
+
+    for (let j = 0; j < losers.length; j++) {
+      if (winners[i] === losers[j]) {
+        count++;
       }
+    }
+
+    switch (count) {
+      case 0:
+        noLossWinners.push(winners[i]);
+      case 1:
+        oneLossWinners.push(winners[i]);
     }
   }
 
@@ -21,3 +33,19 @@ function findWinners(matches: number[][]): number[][] {
 
   return result;
 }
+
+// You also need to include those that have haven't won but have lost.
+console.log(
+  findWinners([
+    [1, 3],
+    [2, 3],
+    [3, 6],
+    [5, 6],
+    [5, 7],
+    [4, 5],
+    [4, 8],
+    [4, 9],
+    [10, 4],
+    [10, 9],
+  ])
+);
