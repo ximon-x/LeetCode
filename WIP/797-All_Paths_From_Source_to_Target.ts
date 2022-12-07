b2db4310a31f3e0afbe351244f2c4d390fc92b2c
Simon Samuel <salviensky@gmail.com>
2022-12-07 08:29:24 +0100

    Understand the question but not a solution

---
+++ WIP/797-All_Paths_From_Source_to_Target.ts
@@ -0,0 +1,16 @@
+function allPathsSourceTarget(graph: number[][]): number[][] {
+	let answer: number[][];
+
+	let start = 0;
+	let end = graph.length - 1;
+
+	
+	// All paths start from node 0
+	
+	
+	// All paths end at node (n - 1)
+
+
+
+	return answer
+}
