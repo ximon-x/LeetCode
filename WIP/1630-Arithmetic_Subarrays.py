edde874a566cf5f07808f8b691157e5b27376681
Simon Samuel <salviensky@gmail.com>
2023-01-03 09:26:28 +0100

    Feat: Solving question 1630

--- WIP/1630-Arithmetic_Subarrays.py
+++ WIP/1630-Arithmetic_Subarrays.py
@@ -3,5 +3,19 @@ class Solution:
         self, nums: list[int], l: list[int], r: list[int]
     ) -> list[bool]:
         result = []
+        m = len(r)
 
+        for i in range(m):
+            curr_nums = nums[l[i] : r[i]]
+            curr_nums.sort()
+
+            diff = curr_nums[1] - curr_nums[0]
+            seq_flag = True
+
+            for j in range(1, len(curr_nums)):
+                if curr_nums[j] - curr_nums[j-1] != diff:
+                    seq_flag = False
+
+            result.append(seq_flag)
+                    
         return result
