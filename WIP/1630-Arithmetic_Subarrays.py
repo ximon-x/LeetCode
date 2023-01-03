b555d636b40012b1a5896f26f00d3d180b927072
Simon Samuel <salviensky@gmail.com>
2023-01-03 05:13:01 +0100

    Fix: Formatted 1630 solution

--- WIP/1630-Arithmetic_Subarrays.py
+++ WIP/1630-Arithmetic_Subarrays.py
@@ -1,5 +1,7 @@
 class Solution:
-    def checkArithmeticSubarrays(self, nums: list[int], l:list[int], r: list[int]) -> list[bool]:
+    def checkArithmeticSubarrays(
+        self, nums: list[int], l: list[int], r: list[int]
+    ) -> list[bool]:
         result = []
 
         return result
