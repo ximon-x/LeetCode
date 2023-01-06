38e126fc22d5023261b9bfa3ed796a5800a520b6
Simon Samuel <salviensky@gmail.com>
2023-01-06 22:53:10 +0100

    Feat: Solved question 1630

--- Medium/1630-Arithmetic_Subarrays.py
+++ Medium/1630-Arithmetic_Subarrays.py
@@ -6,16 +6,16 @@ class Solution:
         m = len(r)
 
         for i in range(m):
-            curr_nums = nums[l[i] : r[i]]
+            curr_nums = nums[l[i] : r[i] + 1]
             curr_nums.sort()
 
             diff = curr_nums[1] - curr_nums[0]
             seq_flag = True
 
             for j in range(1, len(curr_nums)):
-                if curr_nums[j] - curr_nums[j-1] != diff:
+                if curr_nums[j] - curr_nums[j - 1] != diff:
                     seq_flag = False
 
             result.append(seq_flag)
-                    
+
         return result
