ddfff3ecbd197e744c1338bd3a288fb8a7e1dc1d
Simon Samuel <salviensky@gmail.com>
2023-01-15 23:51:15 +0100

    feat: solved question 2427

---
+++ Easy/2427-Number_of_Common_Factors.ts
@@ -0,0 +1,10 @@
+function commonFactors(a: number, b: number): number {
+  // commonFactors  -> Time { O(n), θ(n), Ω(1) } and Space { O(n), θ(n), Ω(1) }
+  let answer = 0;
+
+  for (let i = 0; i <= Math.max(a, b); i++) {
+    if (a % i === 0 && b % i === 0) answer++;
+  }
+
+  return answer;
+}
