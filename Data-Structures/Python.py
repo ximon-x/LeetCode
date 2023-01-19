f73fa24efd810a05e28872bf5f6c2cf1f1a48531
Simon Samuel <salviensky@gmail.com>
2023-01-19 14:26:53 +0100

    chore: added the TreeNode class to python

--- Data-Structures/Python.py
+++ Data-Structures/Python.py
@@ -4,6 +4,13 @@ class ListNode:
         self.next = next
 
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Stack:
     __items = []
     __top = -1
