6fd5d9ca7ae7829579025a9ab6443172ff6811cf
Simon Samuel <salviensky@gmail.com>
2023-01-21 15:01:39 +0100

    feat: solved question 763

---
+++ Medium/763-Partition_Labels.py
@@ -0,0 +1,22 @@
+class Solution:
+    def partitionLabels(self, s: str) -> list[int]:
+        answer = []
+
+        while len(s):
+            if s.count(s[0]) == 1:
+                answer.append(len(s[0]))
+                s = s[1:]
+            
+            else:
+                end = s.rfind(s[0])
+                i = 1
+
+                while i < end:
+                    curr_item_end = s.rfind(s[i])
+                    if curr_item_end > end:
+                        end = curr_item_end
+                    i += 1
+                answer.append(len(s[:end+1]))
+                s = s[end+1:]
+
+        return answer
--- WIP/763-Partition_Labels.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def partitionLabels(self, s: str) -> list[int]:
-        answer = []
-
-        return answer
