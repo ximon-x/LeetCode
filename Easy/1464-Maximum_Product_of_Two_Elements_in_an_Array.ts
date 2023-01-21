9698375b6585013e0d7034e29bb00edd9602fa85
Simon Samuel <salviensky@gmail.com>
2023-01-21 15:19:49 +0100

    fix: previous commit had no code

--- Easy/1464-Maximum_Product_of_Two_Elements_in_an_Array.ts
+++ Easy/1464-Maximum_Product_of_Two_Elements_in_an_Array.ts
@@ -1,7 +1,10 @@
 function maxProduct(nums: number[]): number {
   let answer: number = 0;
+  let n = nums.length;
 
-  nums.sort((a, b) => {a / b})
+  nums.sort((a, b) => a - b)
+
+  answer = (nums[n-1] - 1) * (nums[n-2] - 1)
 
   return answer;
 }
