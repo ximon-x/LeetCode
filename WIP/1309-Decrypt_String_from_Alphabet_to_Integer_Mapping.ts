27f0d3ba8843b68c7f61e5dabb89ede38a79a5d1
Simon Samuel <salviensky@gmail.com>
2023-01-21 15:41:04 +0100

    feat: solving question 1309

---
+++ WIP/1309-Decrypt_String_from_Alphabet_to_Integer_Mapping.ts
@@ -0,0 +1,19 @@
+function freqAlphabets(s: string) :string {
+  let answer = ""
+
+  let alphabets = [...Array(26)].map((_, i) => String.fromCharCode(i + 97));  
+
+  while (s.length) {
+    if (s.length < 3) {
+      answer += alphabets[parseInt(s[0])]
+    }
+    else {
+      if (s[2] === "#") {
+
+      }
+    }
+  }
+
+  return answer;
+
+}
