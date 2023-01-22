bbb0e3936b5e9f68bc9e31f9b49414f42bb7f2d8
Simon Samuel <salviensky@gmail.com>
2023-01-22 10:32:53 +0100

    style: applied prettier formatting

--- WIP/1309-Decrypt_String_from_Alphabet_to_Integer_Mapping.ts
+++ WIP/1309-Decrypt_String_from_Alphabet_to_Integer_Mapping.ts
@@ -1,19 +1,16 @@
-function freqAlphabets(s: string) :string {
-  let answer = ""
+function freqAlphabets(s: string): string {
+  let answer = "";
 
-  let alphabets = [...Array(26)].map((_, i) => String.fromCharCode(i + 97));  
+  let alphabets = [...Array(26)].map((_, i) => String.fromCharCode(i + 97));
 
   while (s.length) {
     if (s.length < 3) {
-      answer += alphabets[parseInt(s[0])]
-    }
-    else {
+      answer += alphabets[parseInt(s[0])];
+    } else {
       if (s[2] === "#") {
-
       }
     }
   }
 
   return answer;
-
 }
