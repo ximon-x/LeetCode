6add628d99861d58c07fdaf587d546d5718fd89e
Simon Samuel <salviensky@gmail.com>
2023-01-22 11:09:03 +0100

    feat: solved leetcode 1309

--- Easy/1309-Decrypt_String_from_Alphabet_to_Integer_Mapping.ts
+++ Easy/1309-Decrypt_String_from_Alphabet_to_Integer_Mapping.ts
@@ -5,9 +5,15 @@ function freqAlphabets(s: string): string {
 
   while (s.length) {
     if (s.length < 3) {
-      answer += alphabets[parseInt(s[0])];
+      answer += alphabets[parseInt(s[0]) - 1];
+      s = s.slice(1);
     } else {
       if (s[2] === "#") {
+        answer += alphabets[parseInt(s.slice(0, 2)) - 1];
+        s = s.slice(3);
+      } else {
+        answer += alphabets[parseInt(s[0]) - 1];
+        s = s.slice(1);
       }
     }
   }
