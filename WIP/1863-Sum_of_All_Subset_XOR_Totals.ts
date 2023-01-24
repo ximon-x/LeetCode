5fa8f813fc721209ac6da82803dbdcf47664e8d6
Simon Samuel <salviensky@gmail.com>
2023-01-24 07:06:55 +0100

    feat: still solving question 1863 but current solution is too hacky

--- WIP/1863-Sum_of_All_Subset_XOR_Totals.ts
+++ WIP/1863-Sum_of_All_Subset_XOR_Totals.ts
@@ -2,8 +2,19 @@ function subsetXORSum(nums: number[]): number {
   if (nums.length == 0) return 0;
   let answer = 0;
 
-  for (let num of nums) {
-    answer = answer ^ num;
+  for (let i = 1; i <= nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      let subsetXOR = 0;
+
+      let subset = nums.slice(j, j + i);
+
+      for (let num of subset) {
+        subsetXOR ^= num;
+      }
+
+      answer += subsetXOR;
+      if (subset.length == nums.length) break;
+    }
   }
 
   return answer;
