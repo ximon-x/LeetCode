1e72e1ed54bc66a385d840919799d5f4c29ff11c
Simon Samuel <salviensky@gmail.com>
2023-01-24 07:23:17 +0100

    refactor: moved to easy and added algorithm analysis

--- Easy/1863-Sum_of_All_Subset_XOR_Totals.ts
+++ Easy/1863-Sum_of_All_Subset_XOR_Totals.ts
@@ -1,4 +1,6 @@
 function subsetXORSum(nums: number[]): number {
+  // subsetXORSum   -> Time { O(n), θ(n), Ω(n) } and Space { O(1), θ(1), Ω(1) }
+
   let answer = 0;
 
   for (let num of nums) answer |= num;
