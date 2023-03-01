c1e08e96fe3ed73e530a835608fb93979e477b2a
Simon Samuel <salviensky@gmail.com>
2023-03-01 09:27:19 +0100

    feat: solving question 2500

---
+++ WIP/2500-Delete_Greatest_Value_in_Each_Row.ts
@@ -0,0 +1,14 @@
+function deleteGreatestValue(grid: number[][]): number {
+  let answer = 0;
+
+  while (grid[0].length !== 0) {
+    let maxNums: number[] = new Array(grid.length, 0);
+
+    for (let i = 0; i < grid[0].length; i++) {
+      if (maxNums[0]) {
+      }
+    }
+  }
+
+  return answer;
+}
