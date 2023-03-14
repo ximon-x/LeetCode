97ec0dd7cb805fda1e5327b0aea9f3b70334bfcf
Simon Samuel <salviensky@gmail.com>
2023-03-14 20:47:17 +0100

    feat: solving question 1827

---
+++ WIP/1827-Minimum_Operations_to_Make_the_Array_Increasing.ts
@@ -0,0 +1,32 @@
+function minOperations(nums: number[]): number {
+  let answer = 0;
+  let maxIndex = nums.indexOf(Math.max(...nums))
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i == maxIndex) continue
+
+    let diff: number = 0;
+    let reqNum: number = 0;
+
+    if (i < maxIndex) {
+      // Difference between i and maxIndex
+      diff = maxIndex - i
+      
+      console.log(diff)
+      // Required number at index i:
+      reqNum = nums[maxIndex] - diff
+    } else if (i > maxIndex) {
+      // Difference between i and maxIndex
+      diff = i - maxIndex
+      
+      // Required number at index i:
+      reqNum = nums[maxIndex] + diff
+    }
+
+    answer += reqNum - nums[i] ;
+  }
+
+  return answer;
+}
+
+console.log(minOperations([1,5,2,4,1]))
