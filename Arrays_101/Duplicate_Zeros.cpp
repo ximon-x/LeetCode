c771ea10b04ce1d0a5fb40835c88c9abbe5730d8
Simon <salviensky@gmail.com>
2023-09-09 12:39:51 +0100

    feat: solved Duplicate_Zeros in C++

---
+++ Arrays_101/Duplicate_Zeros.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Algorithm Analysis
+//      Time Complexity:     Ο(nm) Ω(n)
+//      Space Complexity:    Ο(n) Ω(n) Θ(n)
+
+class Solution {
+   public:
+    void shiftItems(vector<int>& arr, size_t start) {
+        int prev = 0;
+
+        for (size_t i = start; i < arr.size(); i++) {
+            int temp = arr[i];
+            arr[i] = prev;
+            prev = temp;
+        }
+    }
+
+    void duplicateZeros(vector<int>& arr) {
+        size_t n = arr.size();
+        size_t i = 0;
+
+        while (i < n) {
+            if (arr[i] == 0 && i != n - 1) {
+                shiftItems(arr, i + 1);
+                i++;
+            }
+            i++;
+        }
+    }
+};
