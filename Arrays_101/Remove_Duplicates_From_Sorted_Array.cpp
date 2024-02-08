5ce2cf7127e2ad16531e4bc6ff76c80cf1f14cd9
Simon Samuel <salviensky@gmail.com>
2024-02-09 00:24:18 +0100

    feat: solved the problem using 2-pointer

--- Arrays_101/Remove_Duplicates_From_Sorted_Array.cpp
+++ Arrays_101/Remove_Duplicates_From_Sorted_Array.cpp
@@ -1,63 +1,35 @@
-#include <cmath>
 #include <vector>
-#include "../utils/helpers.cpp"
 
 using namespace std;
+// Algorithm Analysis
+//      Time Complexity:     Ο(mn) Ω(1)
+//      Space Complexity:    Ο(1) Ω(1) Θ(1)
 
 class Solution {
    public:
-    void modifyArray(int& p, int& q, vector<int>& nums) {
-        // TODO: Replace items nums(p, q) with nums[q, q + q-p] but consider out
-        // of bounds error.
-        while (p < q) {
-            nums[p] = nums[q + q - p];
-            p++;
-        }
-
-        while (q < nums.size()) {
-            nums[q] = -INFINITY;
-            q++;
-        }
-    }
-
     int removeDuplicates(vector<int>& nums) {
-        if (nums.size() < 2) {
+        if (nums.size() < 2)
             return nums.size();
-        }
 
-        // Number of unique items.
-        int k = 0;
+        int k = 1;
 
-        // Pointers for array access.
-        int p = 0;
-        int q = 1;
+        for (size_t p = 0; p < nums.size() - 1; p++) {
+            for (size_t q = p + 1; q < nums.size(); q++) {
+                if (nums[p] == nums[q]) {
+                    continue;
+                }
 
-        while (p < nums.size() - 1 && nums[p] != -INFINITY) {
-            // If the current number is a duplicate:
-            if (nums[p] == nums[q]) {
-                // Check all succeeding numbers to know if they're duplicates
-                // too.
-                while (nums[p] == nums[q])
-                    q++;
+                if (nums[p] < nums[q]) {
+                    nums[p + 1] = nums[q];
+                    k++;
+                    break;
+                }
+            };
 
-                // Remove all numbers between index p and q
-                modifyArray(p, q, nums);
-                Helpers::printContainer(nums);
-
-                p++;
-                q = p + 1;
-            }
-
-            k++;
+            if (nums[p] == nums[nums.size() - 1])
+                break;
         }
 
         return k;
     }
 };
-
-int main() {
-    vector<int> nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-    Solution solution;
-
-    cout << solution.removeDuplicates(nums);
-}
