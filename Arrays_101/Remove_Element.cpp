6d3a35d222197fdf8ffed72a8b6c7c6c496f8b27
Simon Samuel <salviensky@gmail.com>
2024-02-29 20:23:16 +0100

    chore: optimized the Remove_Element problem

--- Arrays_101/Remove_Element.cpp
+++ Arrays_101/Remove_Element.cpp
@@ -3,32 +3,24 @@
 using namespace std;
 
 // Algorithm Analysis
-//      Time Complexity:     Ο(n + km) Ω(n + k)
+//      Time Complexity:     Ο(n) Ω(n) Θ(n)
 //      Space Complexity:    Ο(1) Ω(1) Θ(1)
 
 class Solution {
    public:
     int removeElement(vector<int>& nums, int val) {
-        size_t k = 0;
-
-        // Find number of unique numbers.
-        for (size_t i = 0; i < nums.size(); i++) {
-            if (nums[i] != val) {
-                k++;
-            }
-        }
-
-        // Populate nums[i..k] with unique numbers.
         size_t i = 0;
-        while (i < k) {
-            for (size_t j = i; j < nums.size(); j++) {
-                if (nums[j] != val) {
-                    nums[i] = nums[j];
-                    i++;
-                }
+        size_t j = 0;
+
+        while (j < nums.size()) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
             }
+
+            j++;
         }
 
-        return k;
+        return i;
     }
 };
