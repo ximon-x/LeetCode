bafcb909183052173786a6a1c67ebbe80fae0f2d
Simon Samuel <salviensky@gmail.com>
2025-01-01 23:41:23 +0100

    feat: solved the Find_Pivot_Index problem

---
+++ Arrays_and_String/Find_Pivot_Index.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include "../utils/helpers.cpp"
+
+using namespace std;
+
+class Solution {
+   public:
+    // Algorithm Analysis
+    //      Time Complexity:     Ο(n) Ω(n) Θ(n)
+    //      Space Complexity:    Ο(1) Ω(1) Θ(1)
+    int pivotIndex(vector<int>& nums) {
+        int leftSum = 0;
+        int totalSum = 0;
+
+        for (auto i = 0; i < nums.size(); i++) {
+            totalSum += nums[i];
+        }
+
+        // We check the sum before each index and compare if it's equal to the
+        // total minus the current element.
+        for (int i = 0; i < nums.size(); i++) {
+            if (leftSum * 2 == totalSum - nums[i]) {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
