fccf8c22e9ba940522813c84bb786690aa6032d9
Simon Samuel <salviensky@gmail.com>
2025-01-07 08:59:48 +0100

    feat: added helper methods for printing maps

--- utils/helpers.cpp
+++ utils/helpers.cpp
@@ -1,14 +1,47 @@
 #include <iostream>
-
-using namespace std;
+#include <map>
+#include <string>
+#include <vector>
 
 struct Helpers {
     template <typename T>
     static void printContainer(T const& range) {
-        cout << " [ ";
+        std::cout << "[ ";
         for (auto const& item : range) {
-            cout << item << ", ";
+            std::cout << item << ", ";
         }
-        cout << "] \n";
+        std::cout << "] \n";
+    }
+
+    static void printMap(const std::map<std::string, int>& m) {
+        std::cout << "{\n";
+        for (const auto& [key, value] : m) {
+            std::cout << "  " << key << ": " << value << "\n";
+        }
+        std::cout << "}\n";
+    }
+
+    static void printMap(const std::map<int, int>& m) {
+        std::cout << "{\n";
+        for (const auto& [key, value] : m) {
+            std::cout << "  " << key << ": " << value << "\n";
+        }
+        std::cout << "}\n";
+    }
+
+    static void printMap(const std::map<int, std::vector<int>>& m) {
+        std::cout << "{\n";
+        for (const auto& [key, vec] : m) {
+            std::cout << "  " << key << ": [";
+
+            for (size_t i = 0; i < vec.size(); ++i) {
+                std::cout << vec[i];
+                if (i < vec.size() - 1) {
+                    std::cout << ", ";
+                }
+            }
+            std::cout << "]\n";
+        }
+        std::cout << "}\n";
     }
 };
